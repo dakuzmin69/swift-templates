ecfa430c1322492ebbe81784395e472e83a5d0bb
Kuzmin Dmitry <33459102+dakuzmin69@users.noreply.github.com>
2022-04-14 09:52:37 +0300

    Update: Complete ModuleView template

--- VIPER module.xctemplate/___FILEBASENAME___Screen/___FILEBASENAME___View.swift
+++ VIPER module.xctemplate/___FILEBASENAME___Screen/___FILEBASENAME___View.swift
@@ -1,3 +1,80 @@
 //___FILEHEADER___
 
-import Foundation
+import UIKit
+
+// MARK: - I___FILEBASENAMEASIDENTIFIER___
+
+protocol I___FILEBASENAMEASIDENTIFIER___: UIView {
+
+    // Set up protocol
+
+}
+
+// MARK: - I___FILEBASENAMEASIDENTIFIER___Delegate
+
+protocol I___FILEBASENAMEASIDENTIFIER___Delegate: AnyObject {
+
+    // Set up protocol
+
+}
+
+// MARK: - ___FILEBASENAMEASIDENTIFIER___
+
+final class ___FILEBASENAMEASIDENTIFIER___: UIView {
+
+    // MARK: Private properties
+
+    private let delegateGetter: () -> I___FILEBASENAMEASIDENTIFIER___Delegate?
+    private var delegate: I___FILEBASENAMEASIDENTIFIER___Delegate? { delegateGetter() }
+
+    // MARK: UI
+
+    // Place the views in this section
+
+    // MARK: Setup UI
+
+    private func setupUI() {
+        setupSuperview()
+        addSubviews()
+        setupLayout()
+    }
+
+    private func setupSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+        // Code to configure the superview
+    }
+
+    private func addSubviews() {
+        // Add subviews to superview
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            // Set up constraints
+        ])
+    }
+
+    // MARK: Init
+
+    init(delegateGetter: @escaping () -> I___FILEBASENAMEASIDENTIFIER___Delegate?) {
+        self.delegateGetter = delegateGetter
+
+        super.init(frame: .zero)
+
+        setupUI()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+// MARK: I___FILEBASENAMEASIDENTIFIER___ implementation
+
+extension ___FILEBASENAMEASIDENTIFIER___: I___FILEBASENAMEASIDENTIFIER___ {
+
+    // Implement I___FILEBASENAMEASIDENTIFIER___ here
+
+}
