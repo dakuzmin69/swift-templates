2c13b8fc791a7e4411405559c32f3964b9f89c7c
Kuzmin Dmitry <33459102+dakuzmin69@users.noreply.github.com>
2022-04-15 23:07:39 +0300

    Add: Router template implementation

--- VIPER module.xctemplate/___FILEBASENAME___Screen/___FILEBASENAME___Router.swift
+++ VIPER module.xctemplate/___FILEBASENAME___Screen/___FILEBASENAME___Router.swift
@@ -1,3 +1,39 @@
 //___FILEHEADER___
 
-import Foundation
+import UIKit
+
+// MARK: ___FILEBASENAMEASIDENTIFIER___
+
+final class ___FILEBASENAMEASIDENTIFIER___ {
+
+    // MARK: Private properties
+
+    private weak var navigationController: UINavigationController?
+
+    // MARK: Init
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+}
+
+// MARK: IRouter implementation
+
+extension ___FILEBASENAMEASIDENTIFIER___: IRouter {
+
+    // Implement IRouter here
+
+}
+
+/*
+
+// MARK: IRouter example
+
+ protocol IRouter {
+
+     func routeToNextScreen()
+
+ }
+
+ */
