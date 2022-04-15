852870b90174b6dd9249735fa0766fd9494325a4
Kuzmin Dmitry <33459102+dakuzmin69@users.noreply.github.com>
2022-04-15 23:20:18 +0300

    Add: Interactor template implementation

--- VIPER module.xctemplate/___FILEBASENAME___Screen/___FILEBASENAME___Interactor.swift
+++ VIPER module.xctemplate/___FILEBASENAME___Screen/___FILEBASENAME___Interactor.swift
@@ -1,3 +1,36 @@
 //___FILEHEADER___
 
 import Foundation
+
+// MARK: - I___FILEBASENAMEASIDENTIFIER___
+
+protocol I___FILEBASENAMEASIDENTIFIER___ {
+
+    // Set up protocol
+
+}
+
+// MARK: - ___FILEBASENAMEASIDENTIFIER___
+
+final class ___FILEBASENAMEASIDENTIFIER___ {
+
+    // MARK: Private properties
+
+    private let presenterGetter: () -> I___VARIABLE_productName:identifier___Presenter?
+    private var presenter: I___VARIABLE_productName:identifier___Presenter? { presenterGetter() }
+
+    // MARK: Init
+
+    init(presenterGetter: @escaping () -> I___VARIABLE_productName:identifier___Presenter?) {
+        self.presenterGetter = presenterGetter
+    }
+
+}
+
+// MARK: ___FILEBASENAMEASIDENTIFIER___ implementation
+
+extension ___FILEBASENAMEASIDENTIFIER___: I___FILEBASENAMEASIDENTIFIER___ {
+
+    // Implement ___FILEBASENAMEASIDENTIFIER___ here
+
+}
